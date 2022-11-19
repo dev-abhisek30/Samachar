aa456dfc5dc25a2c334d1385e0dc15da5743a553
dev-abhisek30 <dev.abhisek30@gmail.com>
2022-11-20 00:07:44 +0530

    FEATURE : Added Main Application

---
+++ app/src/main/java/com/dev/abhisek30/samachar/SamacharApplication.kt
@@ -0,0 +1,14 @@
+package com.dev.abhisek30.samachar
+
+import android.app.Application
+import androidx.viewbinding.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class SamacharApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+}
