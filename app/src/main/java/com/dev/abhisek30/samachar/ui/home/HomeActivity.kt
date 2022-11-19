6ec72103d24d346bb01cd7f122e0be4afc549b7c
dev-abhisek30 <dev.abhisek30@gmail.com>
2022-11-20 01:53:34 +0530

    FEATURE : Updated home and splash activity

--- app/src/main/java/com/dev/abhisek30/samachar/ui/home/HomeActivity.kt
+++ app/src/main/java/com/dev/abhisek30/samachar/ui/home/HomeActivity.kt
@@ -2,11 +2,24 @@ package com.dev.abhisek30.samachar.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.dev.abhisek30.samachar.R
+import com.dev.abhisek30.samachar.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvMain.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/dev/abhisek30/samachar/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/dev/abhisek30/samachar/ui/splash/SplashActivity.kt
@@ -1,11 +1,15 @@
 package com.dev.abhisek30.samachar.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.dev.abhisek30.samachar.BuildConfig
 import com.dev.abhisek30.samachar.R
+import com.dev.abhisek30.samachar.ui.home.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+import kotlin.concurrent.schedule
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
@@ -13,5 +17,12 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+        }, 2000)
+
+        /*Timer().schedule(2000) {
+            startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
+        }*/
     }
 }
